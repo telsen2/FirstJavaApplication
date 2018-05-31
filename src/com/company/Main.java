b4b4712235799c14bcb443edbbbfc75094e74fe8
GBTC441002UR <GBTC441002UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-05-31 15:07:42 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+        System.out.println("Today is May 31 2018. All is good" +
+                " my name is Carlos");
+    }
+}
